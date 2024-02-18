ccb1b22404f97dc6d4b85242a964da3c57f203c8
SaltMeister <huisimonliu@gmail.com>
2024-02-18 11:19:04 -0800

    Initial Commit

---
+++ reciept bill splitter/reciept_bill_splitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  reciept_bill_splitterApp.swift
+//  reciept bill splitter
+//
+//  Created by Simon Huang on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct reciept_bill_splitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
