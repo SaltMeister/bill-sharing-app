e957179f9026e4e3eece325df0114f3e449246a5
SaltMeister <huisimonliu@gmail.com>
2024-02-21 21:17:29 -0800

    Imported Firestore package into project

--- reciept bill splitter/reciept_bill_splitterApp.swift
+++ reciept bill splitter/reciept_bill_splitterApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 struct reciept_bill_splitterApp: App {
+    
+    // Register app delegate for firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
