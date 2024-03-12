3bb1bf674a39ace118c57bf67b350db2c6f8ac8f
jlgvu <102931991+jlgvu@users.noreply.github.com>
2024-03-11 20:48:29 -0700

    Transaction View added

--- reciept bill splitter/HomeView.swift
+++ reciept bill splitter/HomeView.swift
@@ -143,13 +143,6 @@ struct HomeView: View {
     }
 }
 
-struct ToolbarItem: View {
-    let iconName: String
-    let text: String
-    let destination: AnyView
-    
-    
-    
     struct BottomToolbar: View {
         var body: some View {
             NavigationStack{
@@ -184,20 +177,4 @@ struct ToolbarItem: View {
             }
         }
     }
-}
-struct BottomToolbar: View {
-    var body: some View {
-        NavigationStack{
-            HStack(spacing: 0.2) {
-                ToolbarItem(iconName: "person.2", text: "Friends", destination: AnyView(FriendsView()))
-                ToolbarItem(iconName: "person.3", text: "Groups", destination: AnyView(GroupView()))
-                ToolbarItem(iconName: "bolt", text: "Activities", destination: AnyView(HistoryView()))
-                ToolbarItem(iconName: "person.crop.circle", text: "Accounts", destination: AnyView(AccountView()))
-            }
-            .frame(height: 50)
-            .background(Color(UIColor.systemBackground))
-            .cornerRadius(10)
-            .shadow(radius: 3)
-        }
-    }
-}
+
---
+++ reciept bill splitter/TransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionView.swift
+//  reciept bill splitter
+//
+//  Created by Josh Vu on 3/11/24.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionView()
+}
